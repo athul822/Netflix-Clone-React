423373df09d44926184096fc2e33cd208ae4493d
athul822 <mentalist52@gmail.com>
2022-03-25 11:15:53 +0530

    lang drop down as component

--- src/Components/LandingPage/LandingNavBar/LandingNavBar.js
+++ src/Components/LandingPage/LandingNavBar/LandingNavBar.js
@@ -1,5 +1,17 @@
 import React from "react";
 import "./LandingNavBar.css";
+
+export const LangDropDown = ()=>{
+  return(
+  
+    <select className="dropdown-bar" name="lang" id="lang">
+          <option value="">English</option>
+          <option value="">Hindi</option>
+        </select>
+    
+  )
+}
+
 function LandingNavBar() {
   return (
     <div id="navbar">
@@ -9,14 +21,12 @@ function LandingNavBar() {
         alt="netflix-logo"
       />
       <div className="right-nav-el">
-        <select className="dropdown-bar" name="lang" id="lang">
-          <option value="">English</option>
-          <option value="">Hindi</option>
-        </select>
+        <LangDropDown />
         <button className="sign-in-btn">Sign In</button>
       </div>
     </div>
   );
 }
+
 
 export default LandingNavBar;
